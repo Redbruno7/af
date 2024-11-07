4419df81912d5c6e46093120ace9f0361e67cb1d
Redbruno7 <brunorodgers7@gmail.com>
2024-11-07 17:57:45 -0300

    7

---
+++ at_python/exercicio_7.py
@@ -0,0 +1,30 @@
+# TECNÓLOGO DE ANÁLISE E DESENVOLVIMENTO DE SISTEMAS - LÓGICA DE PROGRAMAÇÃO E ALGORITMOS 1
+# ALUNO: BRUNO C. RODGERS
+# DATA: 2024, 2º SEMESTRE
+# EXERCÍCIO 7: Faça um programa que receba a idade de dez pessoas e que calcule e 
+# mostre a quantidade de pessoas com idade maior ou igual a 18 anos.
+
+import os
+
+
+os.system('cls')
+
+# Título
+print('='*70)
+print('EXERCÍCIO 7 - CONTAGEM DE PESSOAS COM 18 ANOS OU MAIS')
+print('='*70)
+
+# Entrada contador
+maioridade = 0
+
+# Processamento
+for i in range(1, 11):
+    idade = int(input(f'Digite a idade da {i}ª pessoa: '))
+    print('-'*70)
+
+    if idade >= 18:
+        maioridade += 1
+
+# Saída
+print(f'Total de pessoas com idade maior ou igual a 18 anos: {maioridade}')
+print('='*70)
