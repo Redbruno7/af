61ee053cb303578e363bc6c918e14a4df93a98b2
Redbruno7 <brunorodgers7@gmail.com>
2024-11-07 19:40:49 -0300

    ajustes e inicio 9

---
+++ at_python/exercicio_9.py
@@ -0,0 +1,16 @@
+# TECNÓLOGO DE ANÁLISE E DESENVOLVIMENTO DE SISTEMAS - LÓGICA DE PROGRAMAÇÃO E ALGORITMOS 1
+# ALUNO: BRUNO C. RODGERS
+# DATA: 2024, 2º SEMESTRE
+# EXERCÍCIO 9:
+
+import os
+
+
+os.system('cls')
+
+# Título
+print('=' * 63)
+print('EXERCÍCIO 9 - ')
+print('=' * 63)
+
+# Entrada
