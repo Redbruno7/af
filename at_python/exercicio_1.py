82daf7b5e28efa111b32bbccab6d7a7e6b0d41b6
Redbruno7 <brunorodgers7@gmail.com>
2024-11-08 18:22:34 -0300

    1 COMPLETO

--- at_python/exercicio_1.py
+++ at_python/exercicio_1.py
@@ -8,16 +8,18 @@ import os
 os.system('cls')
 
 # Título
-print('='*70)
-print('EXERCÍCIO 1')
-print('='*70)
+print('='*60)
+print('EXERCÍCIO 1 - SELEÇÃO DE NÚMEROS EM INTERVALO COM OPERAÇÃO')
+print('='*60)
 
 # Entrada
+print()
+print('=' * 60)
 print('Números entre 1000 e 2000 divisíveis por 11 com resto 5:')
-print('.'*70)
+print('=' * 60)
 
 # Processamento e Saída
 for numero in range(1000, 2000):
     if (numero % 11 == 5):
         print(f'{numero}')
-print('='*70)
+print('='*60)
