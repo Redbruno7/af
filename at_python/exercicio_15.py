8e08973b5e76ece5e4808555ce6bd1cd1b601a28
Redbruno7 <brunorodgers7@gmail.com>
2024-11-08 16:29:53 -0300

    ajustes

---
+++ at_python/exercicio_15.py
@@ -0,0 +1,16 @@
+# TECNÓLOGO DE ANÁLISE E DESENVOLVIMENTO DE SISTEMAS - LÓGICA DE PROGRAMAÇÃO E ALGORITMOS 1
+# ALUNO: BRUNO C. RODGERS
+# DATA: 2024, 2º SEMESTRE
+# EXERCÍCIO 15: Faça um programa que receba dez números e que calcule e mostre a quantidade de números entre 30 e 90
+
+import os
+
+
+os.system('cls')
+
+# Título
+print('='*70)
+print('EXERCÍCIO 15 - VERIFICAÇÃO DE NÚMEROS ENTRE 30 E 90')
+print('='*70)
+
+# Entrada
