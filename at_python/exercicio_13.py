bf4d327a6ee8944487f82b6cc48a35c2d723fd64
Redbruno7 <brunorodgers7@gmail.com>
2024-11-08 12:58:16 -0300

    entrada 13

---
+++ at_python/exercicio_13.py
@@ -0,0 +1,19 @@
+# TECNÓLOGO DE ANÁLISE E DESENVOLVIMENTO DE SISTEMAS - LÓGICA DE PROGRAMAÇÃO E ALGORITMOS 1
+# ALUNO: BRUNO C. RODGERS
+# DATA: 2024, 2º SEMESTRE
+# EXERCÍCIO 13: Faça um programa que receba a idade e o peso de sete pessoas.
+# Calcule e mostre:
+# A quantidade de pessoas com mais de 90 quilos;
+# A média das idades das sete pessoas.
+
+import os
+
+
+os.system('cls')
+
+# Título
+print('=' * 70)
+print('EXERCÍCIO 13 - ')
+print('=' * 70)
+
+# Entrada
