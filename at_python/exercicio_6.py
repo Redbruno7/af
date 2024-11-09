3dce85d4d7affab7adf2d085b0eb9ab863f9c53b
Redbruno7 <brunorodgers7@gmail.com>
2024-11-09 16:29:21 -0300

    6 COMPLETO

--- at_python/exercicio_6.py
+++ at_python/exercicio_6.py
@@ -10,7 +10,7 @@ os.system('cls')
 
 # Título
 print('='*70)
-print('EXERCÍCIO 6')
+print('EXERCÍCIO 6 - SIMULAÇÃO DE LUCRO')
 print('='*70)
 
 # Entrada
@@ -20,32 +20,41 @@ despesa = 200
 reducao_valor = 0.5
 aumento_ingresso = 26
 
+# Contador
 lucro_maximo = 0
 valor_maximo = 0
 ingresso_maximo = 0
 
 # Cabeçalho da tabela
-print(f"{'Preço do ingresso':<25}{'Quantidade Vendida':<25}{'Lucro':<25}")
-print('-'*70)
+print()
+print('='*70)
+print('TABELA DE SIMULAÇÃO DE LUCRO')
+print('='*70)
+print(f"| {'Preço do ingresso':<25}| {'Quantidade Vendida':<25}| {'Lucro':<13}|")
+print('='*70)
 
-# Processamento
-while valor >= 1.00: # Loop para cálculo de lucro no intervalo de 5.00 a 1.00
+# Iteração de R$5,00 a R$1,00
+while valor >= 1.00:
     receita = valor * ingresso
     lucro = receita - despesa
-    print(f'R${valor:<23.2f}{ingresso:<25}{lucro:<13.2f}')
+    print(f'| R${valor:<23.2f}| {ingresso:<25}| {lucro:<13.2f}|')
     
+    # Cálculo dos válores máximos
     if lucro > lucro_maximo:
         lucro_maximo = lucro
         valor_maximo = valor
         ingresso_maximo = ingresso
 
+    # Cálculo proporcional para iteração
     valor -= reducao_valor
     ingresso += aumento_ingresso
+print('=' * 70)
 
 # Saída
-print('-'*70)
+print()
+print('=' * 70)
 print(f'Caso de lucro máximo:')
 print(f'Preço do ingresso: R${valor_maximo:.2f}')
 print(f'Quantidade de ingressos vendidos: {ingresso_maximo}')
 print(f'Lucro: R${lucro_maximo:.2f}')
-print('='*70)
+print('=' * 70)
