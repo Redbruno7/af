99de7bd9ceb2510a72e397fa7f8b452c1d3e3bf6
Redbruno7 <brunorodgers7@gmail.com>
2024-11-09 16:10:58 -0300

    5 COMPLETO

--- at_python/exercicio_5.py
+++ at_python/exercicio_5.py
@@ -9,26 +9,58 @@ import os
 os.system('cls')
 
 # Título
-print('='*70)
-print('EXERCÍCIO 5')
-print('='*70)
+print('=' * 50)
+print('EXERCÍCIO 5 - CÁLCULO DE BÔNUS')
+print('=' * 70)
 
-# Entrada
-num_clientes = 15
+# Lista
+lista_clientes = []
 
-# Processamento e saída
+# Contador
+num_clientes = 3
+
+# Iteração entre 15 clientes
 for i in range(1, num_clientes + 1):
+    print()
+    print('=' * 70)
     print(f'Cliente {i}:')
+    print('=' * 70)
+
+    # Entrada nome
     nome = input('Digite o nome do cliente: ')
-    valor_compras = float(input('Valor total em compras no ano passado: '))
-    
-    if valor_compras < 1000:
-        bonus = valor_compras * 0.1
+    print('-' * 70)
+
+    # Validação de valor de compra
+    while True:
+        valor_compra = float(input('Valor total em compras no ano passado: '))
+        print('=' * 70)
+
+        if valor_compra > 0:
+            break
+        else: 
+            print('Valor inválido! Digite um valor positivo.')
+            print('-' * 70)
+
+    # Cálculo bônus
+    if valor_compra < 1000:
+        bonus = valor_compra * 0.1
     else:
-        bonus = valor_compras * 0.15
-    print('.'*70)
-    print(f'Cliente: {nome}')
-    print(f'Valor em compras no ano passado: R${valor_compras:.2f}')
-    print(f'Bônus especial: R${bonus:.2f}')
-    print('-'*70)
+        bonus = valor_compra * 0.15
+    
+    # Armazenamento na lista
+    lista_clientes.append(
+        {'nome': nome, 'valor_compra': valor_compra, 'bonus': bonus}
+        )
+
+# Saída
+print()
+print('='*70)
+print('RELATÓRIO')
+
+for cliente in lista_clientes:
+    print('='*70)
+    print(f'Cliente: {cliente['nome']}')
+    print(f'Valor em compras no ano passado: R${cliente['valor_compra']:.2f}')
+    print(f'Bônus especial: R${cliente['bonus']:.2f}')
+
 print('='*70)
